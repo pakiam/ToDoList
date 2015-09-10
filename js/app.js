fd801c7936f34f3e8e4364c292dc0aaa23b7a9f7
Maksim <akiam@mail.ru>
2015-09-10 21:56:26 +0300

    adding ToDos function

--- js/app.js
+++ js/app.js
@@ -6,27 +6,51 @@ var main = function () {
         "Go further",
         "Pass session"
     ];
+
     $(".tabs a span").toArray().forEach(function (element) {
         $(element).on("click", function () {
+
             var $element = $(element);
             var $content;
+            var reversedToDos = toDos.reverse();
+            var $emptyInput;
+            var $successInput;
             $(".tabs a span").removeClass("active");
             $element.addClass("active");
             $("main .content").empty();
-            if ($element.parent().is(":nth-child(1)")) {
-                console.log("1st tab");
-            } else if ($element.parent().is(":nth-child(2)")) {
+            function DRY(test) {
                 $content = $("<ul>");
-                toDos.forEach(function (todo) {
+                test.forEach(function (todo) {
                     $content.append($("<li>").text(todo));
                 });
-                $("main .content").append($content);
-            } else if ($element.parent().is(":nth-child(3)")) {
-                console.log("3rd tab");
             }
+
+            if ($element.parent().is(":nth-child(1)")) {
+                DRY(reversedToDos);
+            } else if ($element.parent().is(":nth-child(2)")) {
+                DRY(toDos);
+            } else if ($element.parent().is(":nth-child(3)")) {
+                $content = $("<p>Ваша новая задача: </p><input type='text'> <button class='addTodo'>+</button>");
+                var $toDoInput = $("main .content input");
+                $(".addTodo").on("click", function () {
+
+                    $emptyInput = $("<p style='color: red'>Вы ничего не ввели!</p>");
+                    $successInput = $("<p style='color: green'>Добавлено!</p>");
+                    if ($toDoInput.val() !== "") {
+                        ($successInput).appendTo($("main .content"));
+                        toDos.push($toDoInput.val());
+                        $toDoInput.val("");
+                    } else {
+                        ($emptyInput).appendTo($("main .content"));
+                    }
+                });
+            }
+            $("main .content").append($content);
             return false;
         })
-    })
+    });
+
+    $(".tabs a:first-child span").trigger("click");
 };
 
 $(document).ready(main);
