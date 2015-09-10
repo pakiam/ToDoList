bdd08a70926f4238517d29eb2cfc3e8247c8e0a5
Maksim <akiam@mail.ru>
2015-09-10 20:04:56 +0300

    added func for 2nd tab -> "old tasks" direct order

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
-var main= function () {
-"use strict";
-    for (var i=1;i<=3;i++) {
-        var tabSelector=".tabs a:nth-child("+i+") span";
-        $(tabSelector).on("click", function () {
-            $(".tabs span").removeClass("active");
-            $(this).addClass("active");
-            //$("main .content").empty();
+var main = function () {
+    "use strict";
+
+    var toDos = [
+        "End writing this book",
+        "Go further",
+        "Pass session"
+    ];
+    $(".tabs a span").toArray().forEach(function (element) {
+        $(element).on("click", function () {
+            var $element = $(element);
+            var $content;
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
+            if ($element.parent().is(":nth-child(1)")) {
+                console.log("1st tab");
+            } else if ($element.parent().is(":nth-child(2)")) {
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+                $("main .content").append($content);
+            } else if ($element.parent().is(":nth-child(3)")) {
+                console.log("3rd tab");
+            }
             return false;
-        });
-    }
+        })
+    })
 };
 
 $(document).ready(main);
