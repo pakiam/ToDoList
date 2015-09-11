c31ac0dcdf3cefa0c9a2ca586817d492021d43e2
Maksim <akiam@mail.ru>
2015-09-11 21:10:57 +0300

    some reworks in 1st tab`s function

--- js/app.js
+++ js/app.js
@@ -11,24 +11,23 @@ var main = function () {
 
             var $element = $(element);
             var $content;
-            var reversedToDos = toDos.reverse();
             var $emptyInputText;
             var $successInputText;
             var $input, $button;
             $(".tabs a span").removeClass("active");
             $element.addClass("active");
             $("main .content").empty();
-            function DRY(test) {
-                $content = $("<ul>");
-                test.forEach(function (todo) {
-                    $content.append($("<li>").text(todo));
-                });
-            }
 
             if ($element.parent().is(":nth-child(1)")) {
-                DRY(reversedToDos);
+                $content = $("<ul>");
+                for (var i = toDos.length-1; i >= 0; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
             } else if ($element.parent().is(":nth-child(2)")) {
-                DRY(toDos);
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
             } else if ($element.parent().is(":nth-child(3)")) {
                 $input = $("<input type='text'>");
                 $button = $("<button class='toDoButton'>").text("+");
